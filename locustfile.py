984b10fba945a56ba9d98e7fb4e66d9836a15049
Li Lin <guru.lin@gmail.com>
2021-06-13 13:08:59 +0800

    added logic to load test data fro a CSV file

--- locustfile.py
+++ locustfile.py
@@ -2,9 +2,10 @@ import random
 import re
 import sys
 
-from locust import TaskSet, events, task
+from locust import TaskSet, between, events, task
 from locust.contrib.fasthttp import FastHttpUser
 from locust.main import main
+from data_provider import dataset_for_user
 
 import prometheus_exporter
 
@@ -15,13 +16,14 @@ def init_prometheus(**kwargs):
 
 
 class ApiCallTask(TaskSet):
+    def __init__(self, *args, **kwargs):
+        super(ApiCallTask, self).__init__(*args, **kwargs)
+
     @task
     def get_account_detail(self):
-        self.client.get(f"/accounts/{ self.get_test_id() }")
-
-    def get_test_id(self):
-        i = random.randrange(0, 10)
-        return "11223344" if i > 2 else "gibberish"
+        data = self.user.next_call_params()
+        url = f"/accounts/{ data['account_number'] }"
+        self.client.get(url)
 
 
 class ApiUser(FastHttpUser):
@@ -29,9 +31,13 @@ class ApiUser(FastHttpUser):
 
     def __init__(self, *args, **kwargs):
         super(ApiUser, self).__init__(*args, **kwargs)
+        self.dataset = dataset_for_user()
 
-    def wait_time(self):
-        return 0.2
+    wait_time = between(2, 5)
+
+    def next_call_params(self):
+        r = random.randrange(0, len(self.dataset))
+        return self.dataset[r]
 
 
 if __name__ == "__main__":
