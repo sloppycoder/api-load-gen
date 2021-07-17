e84a703b6563c9c7c338e15cc959c75ef349499b
Mr. Beans <some_email@gmail.com>
2021-07-17 19:21:11 +0800

    added sample custom load shape

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
 import re
 import sys
 
-from locust import TaskSet, between, events, task
+from locust import LoadTestShape, TaskSet, constant_pacing, events, task
 
 # from locust import HttpUser
 from locust.contrib.fasthttp import FastHttpUser as HttpUser
@@ -33,7 +33,41 @@ class ApiUser(HttpUser):
         super(ApiUser, self).__init__(*args, **kwargs)
         self.dataset = []
 
-    wait_time = between(5, 10)
+    wait_time = constant_pacing(0.1)
+
+
+class StagesShape(LoadTestShape):
+    """
+    A simply load test shape class that has different user and spawn_rate at
+    different stages.
+    Keyword arguments:
+        stages -- A list of dicts, each representing a stage with the following keys:
+            duration -- When this many seconds pass the test is advanced to the next stage
+            users -- Total user count
+            spawn_rate -- Number of users to start/stop per second
+            stop -- A boolean that can stop that test at a specific stage
+        stop_at_end -- Can be set to stop once all stages have run.
+    """
+
+    stages = [
+        {"duration": 60, "users": 10, "spawn_rate": 10},
+        {"duration": 120, "users": 20, "spawn_rate": 10},
+        {"duration": 180, "users": 100, "spawn_rate": 10},
+        {"duration": 240, "users": 30, "spawn_rate": 10},
+        {"duration": 300, "users": 10, "spawn_rate": 10},
+        {"duration": 360, "users": 1, "spawn_rate": 1},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                tick_data = (stage["users"], stage["spawn_rate"])
+                print(tick_data)
+                return tick_data
+
+        return None
 
 
 if __name__ == "__main__":
